e9993916afe8917a2be3ad6a167e9a0a9483f48f
Ben Christel <bchristel@pivotal.io>
2020-04-25 15:37:09 -0700

    Refactor test

--- spec/unit/response_for_spec.rb
+++ spec/unit/response_for_spec.rb
@@ -4,31 +4,35 @@ require "in_memory_storage"
 describe "response_for" do
   let(:storage) { InMemoryStorage.new }
 
+  def r(*args)
+    response_for(storage, *args)
+  end
+
   it "responds 404 to a bogus URL" do
-    r = response_for(storage, "GET", "/asdf")
-    expect(r).to eq Response.new(404)
+    expect(r("GET", "/asdf")).to eq Response.new(404)
   end
 
   it "gets the latest revision number" do
-    r = response_for(storage, "GET", "/revisions/latest")
-
-    expect(r).to eq Response.new(
-      200,
-      {"Content-Type" => "application/json"},
-      JSON("id" => 0)
-    )
+    expect(r("GET", "/revisions/latest"))
+      .to eq Response.new(
+        200,
+        {"Content-Type" => "application/json"},
+        JSON("id" => 0)
+      )
   end
 
   it "increments the latest revision number on update" do
-    expect {
-      response_for(storage, "PUT", "/values/foo")
-    }.to change {
-      response_for(storage, "GET", "/revisions/latest").body
-    }.from(JSON "id" => 0).to(JSON "id" => 1)
+    expect(JSON r("GET", "/revisions/latest").body)
+      .to eq "id" => 0
+
+    r("PUT", "/values/foo")
+
+    expect(JSON r("GET", "/revisions/latest").body)
+      .to eq "id" => 1
   end
 
   it "responds 204 to an update" do
-    expect(response_for(storage, "PUT", "/values/foo"))
+    expect(r("PUT", "/values/foo"))
       .to eq Response.new(204)
   end
 end
